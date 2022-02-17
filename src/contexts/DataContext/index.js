58532f86b7a0ec3bbbf4717df1a5aac772dfbcb0
Luís Bragança <l.braganca@runtime-revolution.com>
2022-02-17 17:57:00 +0000

    Removed unnecessary comments

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -7,15 +7,6 @@ export const DataContext = createContext(null);
 export function DataProvider({ children }) {
   const dataStore = useLocalObservable(createDataStore);
 
-  // const [data, dispatch] = useReducer(dataReducer, initialState, () => {
-  //   const localData = localStorage.getItem("data");
-  //   return localData ? JSON.parse(localData) : initialState;
-  // });
-
-  // useEffect(() => {
-  //   localStorage.setItem("data", JSON.stringify(dataStore));
-  // }, [dataStore]);
-
   return (
     <DataContext.Provider value={dataStore}>{children}</DataContext.Provider>
   );
